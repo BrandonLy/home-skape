5aef21d7ba830f7bca317973d2eaf01fdaa2e3b8
RJ Binene <rjbinene98@yahoo.com>
2015-10-08 13:21:14 -0700

    First day-Contact

--- page-contact.php
+++ page-contact.php
@@ -5,15 +5,21 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 <div class="contact-wrap">
 	<h1 class="page-title">Contact Us</h1>
 <div>
 <img src="<?php echo get_site_url(); ?>/wp-content/themes/home-skape/img/asset5.png">
-<p>Call Us</p> <!--Phone Number-->
+	<p>Call Us</p> <!--Phone Number-->
 <img src="<?php echo get_site_url(); ?>/wp-content/themes/home-skape/img/asset6.png">
-<p>Email</p> /*Email*/
+	<p>Email</p> <!--Email-->
+</div>
+<div class="section-wrap">
+	<h1 class="section-title">Find Us</h1>
+</div>
+<div class="map-wrap">
+<!--Map Address-->
 </div>
 </div>
 <?php get_footer(); ?>
