16e4a053ee69d017cc55cc3631b56525b3fc4429
RJ Binene <rjbinene98@yahoo.com>
2015-10-08 12:59:18 -0700

    updating contact page

--- page-contact.php
+++ page-contact.php
@@ -7,22 +7,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+<div class="contact-wrap">
+	<h1 class="page-title">Contact Us</h1>
+<div>
+<img src="<?php echo get_site_url(); ?>/wp-content/themes/home-skape/img/asset5.png">
+<p>Call Us</p> <!--Phone Number-->
+<img src="<?php echo get_site_url(); ?>/wp-content/themes/home-skape/img/asset6.png">
+<p>Email</p> /*Email*/
+</div>
+</div>
 <?php get_footer(); ?>
