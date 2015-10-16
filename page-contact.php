d54cc916771f901026f76d6eac6c51fe6b12431f
Brandon Ly <lypanda1@gmail.com>
2015-10-16 11:14:27 -0700

    Commit

--- page-contact.php
+++ page-contact.php
@@ -9,8 +9,9 @@ get_header(); ?>
 	<main id="main" class="site-main" role="main">
 <div class="contact-wrap">
 	<h1 class="page-title">Contact Us</h1>
-<div>
+<div class="call-asset">
 <img src="<?php echo get_site_url(); ?>/wp-content/themes/home-skape/img/asset5.png">
+<<<<<<< Updated upstream
 	<p>Call Us</p> <!--Phone Number-->
 <img src="<?php echo get_site_url(); ?>/wp-content/themes/home-skape/img/asset6.png">
 	<p>Email</p> <!--Email-->
@@ -20,6 +21,15 @@ get_header(); ?>
 </div>
 <div class="map-wrap">
 <!--Map Address-->
+=======
+<p>Call Us</p> <!--Phone Number Needed-->
+<div class="email-asset">
+<img src="<?php echo get_site_url(); ?>/wp-content/themes/home-skape/img/asset6.png">
+<p>Email</p> <!--Email Needed--> 
+</div>
+<div class="map-locations">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4422.558742423578!2d-115.24843561790313!3d36.03734484611808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c8c87af578d6b3%3A0xa6bb42c80b094cb4!2sSouthwest+Career+and+Technical+Academy!5e0!3m2!1sen!2sus!4v1444678487316" width="600" height="450" frameborder="0" style="border: 0;" allowfullscreen></iframe>
+>>>>>>> Stashed changes
 </div>
 </div>
 <?php get_footer(); ?>
