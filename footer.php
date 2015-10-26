6243246df1a246b968bfa71271ea4b7b52634276
Brandon Ly <lypanda1@gmail.com>
2015-10-26 12:19:44 -0700

    styled home page

--- footer.php
+++ footer.php
@@ -14,10 +14,19 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div id="footer-title">
+			<img id="footer-logo" src="<?php echo get_site_url() ?>/wp-content/themes/home-skape/img/logo.png">
+			<h6>HOMESKAPE</h6>
+		</div>
+		<nav id="footer-nav" role="navigation">
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'home-skape' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'home-skape' ), 'WordPress' ); ?></a>
+			<p id="created-by">Website created by <a href="">Studio C</a></p>
+			<p id="copyright">copyright 2014 Homeskape</p>
+			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'home-skape' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'home-skape' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'home-skape' ), 'home-skape', '<a href="http://swcta-studioc.github.io" rel="designer">StudioC</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'home-skape' ), 'home-skape', '<a href="http://swcta-studioc.github.io" rel="designer">StudioC</a>' ); ?> -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- front-page.php
+++ front-page.php
@@ -35,36 +35,15 @@ get_header(); ?>
 			</div>
 		</div>
 
-		<?php if ( have_posts() ) : ?>
+		<div id="about-wrap">
+			<div id="about">
+				<h2 id="about-title">ABOUT US</h2>
+				<p>Homeskape is a startup company made in 1994 blah growl hop down the river in a camel girl. Homeskape is a company we are making a website for them I hope they like my filler text because nobody has any content.</p>
+			</div>
+			<img src="<?php echo get_site_url() ?>/wp-content/themes/home-skape/img/logo.png">
+		</div>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
+		
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
